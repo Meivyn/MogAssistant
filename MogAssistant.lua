0328fb2b6bc94165441f404922a5a276a80ae2ab
Meivyn <meivyn@live.ca>
2019-11-17 15:24:03 -0500

    update: partial rework of the add-on

--- MogAssistant.lua
+++ MogAssistant.lua
@@ -1,7 +1,10 @@
 local _G = _G
+local ipairs = ipairs
+local select = select
 
+local C_TransmogCollection_GetAppearanceSourceInfo = C_TransmogCollection.GetAppearanceSourceInfo
+local C_TransmogCollection_GetIllusionSourceInfo = C_TransmogCollection.GetIllusionSourceInfo
 local CreateFrame = CreateFrame
-local hooksecurefunc = hooksecurefunc
 local DressUpVisual = DressUpVisual
 local PlaySound = PlaySound
 
@@ -13,37 +16,16 @@ local WARDROBE_NEXT_VISUAL_KEY = WARDROBE_NEXT_VISUAL_KEY
 local WARDROBE_PREV_VISUAL_KEY = WARDROBE_PREV_VISUAL_KEY
 local WARDROBE_UP_VISUAL_KEY = WARDROBE_UP_VISUAL_KEY
 
-local function HandleDressing(self)
-  local GetWeaponInfoForEnchant = WardrobeCollectionFrame_GetWeaponInfoForEnchant
-  local isCategoryLegionArtifact = WardrobeUtils_IsCategoryLegionArtifact(self:GetActiveCategory())
-  local isCategoryRanged = WardrobeUtils_IsCategoryRanged(self:GetActiveCategory())
-
-  local transmogType = self.transmogType
-  local slot = self:GetActiveSlot()
-  if transmogType == LE_TRANSMOG_TYPE_APPEARANCE then
-    local sourceID = self:GetAnAppearanceSourceFromVisual(self.filteredVisualsList[self.visualIndex].visualID, nil)
-    -- don't specify a slot for ranged weapons
-    if isCategoryRanged or isCategoryLegionArtifact then
-      slot = nil
-    end
-    DressUpVisual(sourceID, slot)
-  elseif transmogType == LE_TRANSMOG_TYPE_ILLUSION then
-    local weaponSourceID = GetWeaponInfoForEnchant(slot)
-    DressUpVisual(weaponSourceID, slot, self.filteredVisualsList[self.visualIndex].sourceID)
-  end
-end
-
-local function HandleBorder(self)
+local function UpdateItems(self)
   local pendingTransmogModelFrame
-  local index
   local indexOffset = (self.PagingFrame:GetCurrentPage() - 1) * self.PAGE_SIZE
   for i = 1, self.PAGE_SIZE do
     local model = self.Models[i]
-    index = i + indexOffset
+    local index = i + indexOffset
     local visualInfo = self.filteredVisualsList[index]
     if visualInfo then
       local transmogStateAtlas
-      if visualInfo.visualID == self.filteredVisualsList[self.visualIndex].visualID then
+      if visualInfo.visualID == self.selectedVisualID then
         transmogStateAtlas = "transmog-wardrobe-border-selected"
         pendingTransmogModelFrame = model
       end
@@ -53,63 +35,81 @@ local function HandleBorder(self)
       end
     end
   end
-  self.pageIndex = index
   if pendingTransmogModelFrame then
     self.PendingTransmogFrame:SetParent(pendingTransmogModelFrame)
     self.PendingTransmogFrame:SetPoint("CENTER")
     self.PendingTransmogFrame:Show()
-    self.PendingTransmogFrame.TransmogSelectedAnim:Stop()
-    self.PendingTransmogFrame.TransmogSelectedAnim:Play()
-    self.PendingTransmogFrame.TransmogSelectedAnim2:Stop()
-    self.PendingTransmogFrame.TransmogSelectedAnim2:Play()
-    self.PendingTransmogFrame.TransmogSelectedAnim3:Stop()
-    self.PendingTransmogFrame.TransmogSelectedAnim3:Play()
-    self.PendingTransmogFrame.TransmogSelectedAnim4:Stop()
-    self.PendingTransmogFrame.TransmogSelectedAnim4:Play()
-    self.PendingTransmogFrame.TransmogSelectedAnim5:Stop()
-    self.PendingTransmogFrame.TransmogSelectedAnim5:Play()
+    if self.PendingTransmogFrame.visualID ~= self.selectedVisualID then
+      self.PendingTransmogFrame.TransmogSelectedAnim:Stop()
+      self.PendingTransmogFrame.TransmogSelectedAnim:Play()
+      self.PendingTransmogFrame.TransmogSelectedAnim2:Stop()
+      self.PendingTransmogFrame.TransmogSelectedAnim2:Play()
+      self.PendingTransmogFrame.TransmogSelectedAnim3:Stop()
+      self.PendingTransmogFrame.TransmogSelectedAnim3:Play()
+      self.PendingTransmogFrame.TransmogSelectedAnim4:Stop()
+      self.PendingTransmogFrame.TransmogSelectedAnim4:Play()
+      self.PendingTransmogFrame.TransmogSelectedAnim5:Stop()
+      self.PendingTransmogFrame.TransmogSelectedAnim5:Play()
+    end
+    self.PendingTransmogFrame.visualID = self.selectedVisualID
   end
 end
 
-local function CanHandleKey(key)
-  local isAtTransmogrifier = WardrobeFrame_IsAtTransmogrifier()
-
-  return not isAtTransmogrifier and _G.WardrobeCollectionFrame.selectedTab == 1 and (key == WARDROBE_PREV_VISUAL_KEY or key == WARDROBE_NEXT_VISUAL_KEY or key == WARDROBE_UP_VISUAL_KEY or key == WARDROBE_DOWN_VISUAL_KEY)
+local function DressUp(self, visualInfo)
+  local transmogType = self.transmogType
+  local slot = self:GetActiveSlot()
+  if transmogType == LE_TRANSMOG_TYPE_APPEARANCE then
+    local sourceID = self:GetAnAppearanceSourceFromVisual(visualInfo.visualID, nil)
+    if WardrobeUtils_IsCategoryRanged(self:GetActiveCategory()) or WardrobeUtils_IsCategoryLegionArtifact(self:GetActiveCategory()) then
+      slot = nil
+    end
+    DressUpVisual(sourceID, slot)
+    self.selectedVisualID = select(2, C_TransmogCollection_GetAppearanceSourceInfo(sourceID))
+  elseif transmogType == LE_TRANSMOG_TYPE_ILLUSION then
+    local weaponSourceID = WardrobeCollectionFrame_GetWeaponInfoForEnchant(slot)
+    DressUpVisual(weaponSourceID, slot, visualInfo.sourceID)
+    self.selectedVisualID = C_TransmogCollection_GetIllusionSourceInfo(visualInfo.sourceID)
+  end
 end
 
-local function HandleKey(self, key)
-  local isAtTransmogrifier = WardrobeFrame_IsAtTransmogrifier()
-  local GetAdjustedDisplayIndexFromKeyPress = WardrobeUtils_GetAdjustedDisplayIndexFromKeyPress
-
-  if CanHandleKey(key) then
+local function OnKeyDown(self, key)
+  if WardrobeFrame_IsAtTransmogrifier() then return end
+  if key == WARDROBE_PREV_VISUAL_KEY or key == WARDROBE_NEXT_VISUAL_KEY or key == WARDROBE_UP_VISUAL_KEY or key == WARDROBE_DOWN_VISUAL_KEY then
     self:SetPropagateKeyboardInput(false)
-    local visualIndex = self.ItemsCollectionFrame.visualIndex
-    local visualsList = self.ItemsCollectionFrame:GetFilteredVisualsList()
-    visualIndex = GetAdjustedDisplayIndexFromKeyPress(self.ItemsCollectionFrame, visualIndex, #visualsList, key)
-    self.ItemsCollectionFrame.visualIndex = visualIndex
+    local visualIndex = 0
+    local visualsList = self.activeFrame.filteredVisualsList
+    for i = 1, #visualsList do
+      if visualsList[i].visualID == self.activeFrame.selectedVisualID then
+        visualIndex = i
+        break
+      end
+    end
+    visualIndex = WardrobeUtils_GetAdjustedDisplayIndexFromKeyPress(self.activeFrame, visualIndex, #visualsList, key)
     PlaySound(SOUNDKIT_UI_TRANSMOG_ITEM_CLICK)
-    self.ItemsCollectionFrame.jumpToVisualID = visualsList[visualIndex].visualID
-    self.ItemsCollectionFrame:ResetPage()
-    HandleDressing(self.ItemsCollectionFrame)
-    HandleBorder(self.ItemsCollectionFrame)
-  elseif not isAtTransmogrifier then
+    self.activeFrame.jumpToVisualID = visualsList[visualIndex].visualID
+    self.activeFrame.selectedVisualID = visualsList[visualIndex].visualID
+    self.activeFrame:ResetPage()
+    DressUp(self.activeFrame, visualsList[visualIndex])
+    UpdateItems(self.activeFrame)
+  else
     self:SetPropagateKeyboardInput(true)
   end
+end
+
+local function OnMouseDown(self)
+  if WardrobeFrame_IsAtTransmogrifier() then return end
+  PlaySound(SOUNDKIT_UI_TRANSMOG_ITEM_CLICK)
+  DressUp(self:GetParent(), self.visualInfo)
+  UpdateItems(self:GetParent())
 end
 
 local frame = CreateFrame("Frame")
 frame:RegisterEvent("ADDON_LOADED")
 frame:SetScript("OnEvent", function(_, event, arg1)
   if event == "ADDON_LOADED" and arg1 == "Blizzard_Collections" then
-    _G.WardrobeCollectionFrame:HookScript("OnKeyDown", HandleKey)
-    hooksecurefunc(_G.WardrobeCollectionFrame.ItemsCollectionFrame, "SetActiveCategory", function(self)
-      self.visualIndex = 0
-    end)
-    hooksecurefunc(_G.WardrobeCollectionFrame.ItemsCollectionFrame, "OnPageChanged", function(self, userAction)
-      if userAction then
-        --TODO: This doesn't work as desired when the user changes pages more than once
-        self.visualIndex = self.pageIndex
-      end
-    end)
+    _G.WardrobeCollectionFrame:HookScript("OnKeyDown", OnKeyDown)
+    for _, model in ipairs(_G.WardrobeCollectionFrame.ItemsCollectionFrame.Models) do
+      model:HookScript("OnMouseDown", OnMouseDown)
+    end
   end
 end)
